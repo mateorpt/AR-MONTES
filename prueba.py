2e811f6970e2eb85425af54965fe8f88fd9701f6
mateorpt <mmontes240@misena.edu.co>
2022-03-04 22:48:42 -0500

    prueba de carga

---
+++ prueba.py
@@ -0,0 +1,32 @@
+#ejemplo1
+def func():
+    print("este es un numero func()")
+
+def main():
+    print("esto es una palabra main()")
+
+if __name__ == "__main__":
+    main()   
+#ejemplo2
+def men():
+    print("""             combierta la unidad de tiempo que desea:
+        1) horas a minutos.
+        2) horas a segundos.
+        3) minutos a horas.
+        4) minutos a segundos. 
+        5) segundos a minutos.
+        6) segundos a horas""") 
+    
+if __name__ == "__men__":
+    men()
+#ejemplo3
+a = 156
+b = 489
+def suma():
+    print(a+b)
+
+
+    
+    
+
+    
