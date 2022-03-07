c33acfb4d4d0ac4cc4702140e49e6f6145bb85ac
mateorpt <mmontes240@misena.edu.co>
2022-03-06 20:49:19 -0500

    cambios realizadoss

---
+++ taller.py
@@ -0,0 +1,5 @@
+def suma(A, B):
+    return A+B
+
+def resta(C, D):
+    return C+D
---
+++ test_suma-de-dos-factores.py
@@ -0,0 +1,11 @@
+import pytest
+import taller
+
+def test_suma():
+    assert taller.suma(20 , 10) == 30 
+    
+def test_resta():
+    assert taller.resta(20 , 10) == 0 
+
+
+
